de3b443885da1e934ab48676522572441d2db3a9
vpelkonen <lumisielu@gmail.com>
2016-01-23 00:52:35 +0200

    Styled, added digital display to timer, pause/play on click.

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 
 const Timer = React.createClass({
 	getInitialState: function(){
-		return {secondsElapsed: 0};
+		return {
+			timerOn: false,
+			secondsElapsed: 0 // 5 = 25 min
+		};
 	},
 	tick: function(){
-		this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });
+		if(this.state.timerOn && this.state.secondsElapsed != 5){
+			this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });
+		}
+		else if(this.state.timerOn && this.state.secondsElapsed >= 5){
+			alert("YO");
+			this.setState({ timerOn: false });
+		}
 	},
 	componentDidMount: function(){
 		this.interval = setInterval(this.tick, 1000);
@@ -14,13 +23,17 @@ const Timer = React.createClass({
 	componentWillUnmount: function(){
 		clearInterval(this.interval);
 	},
-	handleClick(e){
-
+	fixDigit: function(str){
+    	return (new Array(3).join('0')+str).slice(-2);
+	},
+	handleClick: function(e){
+		this.setState({timerOn: !this.state.timerOn});
 	},
 	render(){
+		let minutes = Math.floor((5 - this.state.secondsElapsed) / 60);
+		let seconds = this.fixDigit((5 - this.state.secondsElapsed) - minutes * 60);
 		return(
-			<div> Timer: {this.state.secondsElapsed}</div>
-/*			<button onClick={ this.handleClick } className="timerButton"></button>*/
+			<button className="timerClock" onClick={ this.handleClick }> {minutes + ":" + seconds}</button>
 		);
 	}
 });
